537529b64139d080e791ea600672423003d6bd65
Anshi Agarwal <anshi110312@gmail.com>
2023-07-22 23:02:04 +0530

    Initial Commit

---
+++ app/src/main/java/com/mastercoding/note_taking_app/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mastercoding.note_taking_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+
+//Successfully created project 'Note_Taking_App' on GitHub, but initial commit failed: Author identity unknown *** Please tell me who you are. Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'anshi@MSI.(none)')
